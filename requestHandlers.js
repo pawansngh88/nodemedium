3021da7ff43d078c06218f0c6e1a217025dd76a7
Pawan Singh <pawansngh88@gmail.com>
2013-08-11 00:54:34 +0530

    Added html to req handler

--- requestHandlers.js
+++ requestHandlers.js
@@ -2,19 +2,21 @@ var exec = require("child_process").exec;
 
 function start(res) {
   console.log("Request handler 'start' was called.");
-  /*function sleep(milliSeconds) {
-    var startTime = new Date().getTime();
-    while (new Date().getTime() < startTime + milliSeconds);
-  }
-  sleep(10000);
-  return "Hello start";*/
-  var content = "empty";
-  exec("ls -lah", function (error, stdout, stderr) {
-      res.writeHead(200,{"Content-Type": "text/plain; charset=utf-8"});
-      res.write(stdout);
-      res.end();
-      });
-  return content;
+  var body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; '+
+    'charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" method="post">'+
+    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<input type="submit" value="Submit text" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
+  res.writeHead(200,{"Content-Type": "text/plain; charset=utf-8"});
+  res.write(body);
+  res.end();
 }
 function upload(res) {
   console.log("Request handler 'upload' was called.");
