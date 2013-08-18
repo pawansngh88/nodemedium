9397572daa4bbfe8c48f79cfdc20a2962a3cd533
Pawan Singh <pawansngh88@gmail.com>
2013-08-18 15:00:29 +0530

    Added image file display

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,5 +1,6 @@
 var exec = require("child_process").exec;
 var querystring = require("querystring");
+var fs = require("fs");
 
 
 function start(res, postData) {
@@ -27,6 +28,28 @@ function upload(res, postData) {
   res.end();
 }
 
+function show(res, postData) {
+  console.log("Request handler 'show' was called.");
+  fs.readFile("/tmp/test.png", function(error, file1) {
+  if (error) {
+    console.log("'show':"+error);
+    res.writeHead(500, {"Content-Type": "text/plain; charset=utf-8"});
+    res.write(error + "\n");
+    res.end();
+  }  else {
+    console.log("'show': No Error"+ file1);
+    //res.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});
+    //res.write("Sending png file: ");
+    res.writeHead(200, {"Content-Type": "image/png"});
+    //res.write("<img src='data:image/png;base64,"+file+"'/>");
+    res.write(file1, "binary");
+    //res.end(file1); also works
+    res.end();
+    return;
+    }
+  });
+}
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
 
